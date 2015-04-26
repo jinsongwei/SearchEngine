e998f9d356a8d391b113aea87432846ba54e4c16
jinsongwei <songweijin2012@gmail.com>
2015-04-26 02:48:15 -0700

    update Main

--- com/company/Main.java
+++ com/company/Main.java
@@ -89,16 +89,7 @@ public class Main {
             thr.setPriority(9);
             thr.start();
             name++;
-          //  count++;
         }
-        /*
-        while(true){
-            checkSize();
-            System.out.println("data size : " + dataSize);
-            Thread.sleep(5000);
-        }
-        */
-
     }
     public static void checkSize(){
         if(dataSize > LIMIT) {
@@ -110,95 +101,6 @@ public class Main {
         f.createNewFile();
     }
 
-    /* param: the url
-        return: a vector contain all links in this url
-     */
-/*    public static Vector<String> passUrl(String url)throws IOException{
-        Vector<String> v = new Vector<String>();
-
-
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("a[href]");
-        Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
-
-        String text = doc.body().text();
-
-        //create_file(fileName);
-        //PrintWriter writer = new PrintWriter(fileName);
-       // writer.println(text);
-        //writer.close();
-
-        for(Element link : links){
-            String urls = link.attr("abs:href");
-            v.add(urls);
-            String pathTemp = path + Integer.toString(docNum) + ".txt";
-            System.out.println(urls + "\t" + link.text());
-            MyThreads t = new MyThreads(pathTemp,urls,link.text());
-            t.run();
-            docNum++;
-        }
-        return v;
-    }
-*/
-    public static void start(String urlTemp,String fileN)throws IOException{
-        //Validate.isTrue(args.length == 1, "usage: supply url to fetch");
-        String url = urlTemp;
-
-
-
-        //        "http://www.ucr.edu";
-        print("Fetching %s...", url);
-
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("a[href]");
-        Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
-
-        String text = doc.body().text();
-        print("\nMedia: (%d)", media.size());
-        for (Element src : media) {
-            if (src.tagName().equals("img"))
-                print(" * %s: <%s> %sx%s (%s)",
-                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-                        trim(src.attr("alt"), 20));
-            else
-                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
-        }
-
-        print("\nImports: (%d)", imports.size());
-        for (Element link : imports) {
-            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
-        }
-
-        print("\nLinks: (%d)", links.size());
-
-        try(BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-        }
-        String everything = sb.toString();
-    }
-/*
-        for (Element link : links) {
-            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
-        }
-*/
-
-        print("\ntext: ");
-
-       // print(" ******** :(%s)",text);
-
-    }
-
-    private static void print(String msg, Object... args) {
-        System.out.println(String.format(msg, args));
-    }
 
     private static String trim(String s, int width) {
         if (s.length() > width)
